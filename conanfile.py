4efc3c20546cd01c8fe3d0eac0cf3233669bc093
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2021-05-31 17:15:32 +0100

    Create a build for the debug build vmware sent

--- conanfile.py
+++ conanfile.py
@@ -39,7 +39,8 @@ class InstallBuilderConan(ConanFile):
                     '-mountpoint', os.path.join(self.build_folder, 'mnt')
                 ])
                 subprocess.check_call([
-                    os.path.join('mnt', f'installbuilder-qt-professional-{self.version}-osx-installer.app','Contents','MacOS','osx-x86_64'),
+                    # os.path.join('mnt', f'installbuilder-qt-professional-{self.version}-osx-installer.app','Contents','MacOS','osx-x86_64'),
+                    os.path.join('mnt', f'installbuilder-qt-professional-21.3.0-osx-installer.app','Contents','MacOS','osx-x86_64'),
                     '--mode', 'unattended',
                     '--prefix', 'inst', 
                 ])
